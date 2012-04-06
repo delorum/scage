ad31ccb408571a9e41959ec60857e2bb00fc4be2
dunnololda <dunnolol.b@gmail.com>
2012-04-06 09:32:12 +0400

    added restitution option for StaticLine

--- src/main/scala/net/scage/support/physics/objects/StaticLine.scala
+++ src/main/scala/net/scage/support/physics/objects/StaticLine.scala
@@ -5,12 +5,13 @@ import net.phys2d.math.Vector2f
 import net.phys2d.raw.StaticBody
 import _root_.net.scage.support.Vec
 import net.scage.support.physics.Physical
+import _root_.net.scage.support.ScageProperties._
 
-class StaticLine(start:Vec, end:Vec) extends Physical {
+class StaticLine(start:Vec, end:Vec, restitution:Boolean = property("physics.restitution", false)) extends Physical {
   val line = new Line((end-start).x, (end-start).y)
 
   val body = new StaticBody("line", line)
-  body.setRestitution(1.0f)
+  if(restitution) body.setRestitution(1.0f)
   body.setPosition(start.x, start.y)
 
   def points = {
