1f508426b66fd8d65a7389d80c0f92b73d7fa286
Andrey Borunov <a.borunov@netris.ru>
2013-08-12 18:56:30 +0400

    improved Vec and DVec with several methods

--- src/main/scala/com/github/dunnololda/scage/support/Vec.scala
+++ src/main/scala/com/github/dunnololda/scage/support/Vec.scala
@@ -44,7 +44,10 @@ class Vec(val x:Float = 0, val y:Float = 0) {
   def +(v:Vec) = new Vec(x+v.x, y+v.y)
   def -(v:Vec) = new Vec(x-v.x, y-v.y)
 
+  def unary_-():Vec = new Vec(-x, -y)
+
   def *(v:Vec) = x*v.x + y*v.y
+  def */(v:Vec) = x*v.y - y*v.x
 
   def *(k:Double) = new Vec(x*k, y*k)
   def *(k:Float)  = new Vec(x*k, y*k)
@@ -126,7 +129,10 @@ class DVec(val x:Double = 0, val y:Double = 0) {
   def +(dv:DVec)   = new DVec(x+dv.x, y+dv.y)
   def -(dv:DVec)   = new DVec(x-dv.x, y-dv.y)
 
+  def unary_-():DVec = new DVec(-x, -y)
+
   def *(dv:DVec)   = x*dv.x + y*dv.y
+  def */(v:Vec) = x*v.y - y*v.x
 
   def *(k:Double) = new DVec(x*k, y*k)
   def *(k:Float)  = new DVec(x*k, y*k)
